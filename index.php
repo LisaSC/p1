bf81ac768ef831eb3b85d9de8c0d190d12f83ba4
LisaSC <ink@mit.edu>
2014-06-28 18:51:04 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DWA Project 1</title>
+</head>
+
+<body>
+<h1>
+Lisa Sukharev-Chuyan
+</h1>
+	<p>
+	I am a rising junior in college switching into a CS major and scrambling to	catch up on some CS knowledge. So far, I have done some coding in Lisp 
+	(Scheme), Python, Java, and BASIC. As of a few minutes ago, I have created a web page in HTML. I run Windows 7 (and hope to switch to Linux of course).
+	The typical biography makes the assumption that the readers are looking for	its subject, so my suggestion to you, my readers, is to start looking in the
+	Charles River near MIT, probably on the capsized boat. If that isn't successful, I might be climbing, but I'm not suggesting you follow me due to 
+	liability issues.
+	</p>
+</body>
+</html>
