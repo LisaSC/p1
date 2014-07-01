ede58061609e70efa7dab4c90d22c4c8448e820d
LisaSC <ink@mit.edu>
2014-07-01 18:19:59 -0400

    added images and styling

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <style>
+	body{text-align:center}
+	p{text-align:justify}
+  </style>
   <title>DWA Project 1</title>
   <meta charset='utf-8'>
 </head>
 
-<body>
+<body style="text-align:center">
 <h1>Lisa Sukharev-Chuyan</h1>
-	<p>
+
+<img src='/images/portrait.png' alt='portrait'>
+<img src='/images/sailing.png' alt='sailing'>
+	<p>	
 	I am a rising junior in college switching into a CS major and scrambling to catch up on some CS knowledge. So far, I have done some coding in Lisp
 	(Scheme), Python, Java, and BASIC. As of a few minutes ago, I have created a web page in HTML. I run Windows 7 (and hope to switch to Linux of course).
 	The typical biography makes the assumption that the readers are looking for its subject, so my suggestion to you, my readers, is to start looking in the
 	Charles River near MIT, probably on the capsized boat. If that isn't successful, I might be climbing, but I'm not suggesting you follow me due to
 	liability issues.
 	</p>
+<img src='/images/summitRidge.png' alt='summit ridge'>
+                
+<img src='/images/silhouette.png' alt='silhouette'>
 </body>
 </html>
